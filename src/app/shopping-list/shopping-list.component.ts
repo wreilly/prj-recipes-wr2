a4c5e4a32f8d05f6e8902b7de058722a3bdb4c60
William Reilly <wreilly2001@gmail.com>
2019-11-15 07:15:22 -0500

    Lecture 224 TD Form working

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,9 +9,14 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
+
   ingredients: Ingredient[];
+  ingredientSelectedToEdit: Ingredient;
 
   myIngredientsChangedSubscription = new Subscription();
+/* No. Not needed in this component. From here we SEND (.next), not Subscribe. Cheers. (We Subscribe over in ShoppingEditComponent)
+  myIngredientSelectedToEditSubscription = new Subscription();
+*/
 
   constructor(private slService: ShoppingListService) { }
 
@@ -23,12 +28,40 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     this.myIngredientsChangedSubscription = this.slService.myIngredientsChangedSubject
       .subscribe(
         (ingredients: Ingredient[]) => {
+          console.log('ingredients are now ', ingredients);
           this.ingredients = ingredients;
         }
       );
+
+/* No - There Is No Need for this Shopping List Component
+to subscribe to this Subject. Nope.
+Here in the List, we SEND (.next()) TO that Subject, over
+on the Service
+Let OTHER Components (like Shopping Edit Component) do
+the Subscribing etc.
+
+    this.myIngredientSelectedToEditSubscription = this.slService.myIngredientToEdit.subscribe((ingredientToEditWeGot) => {
+      this.ingredientSelectedToEdit = ingredientToEditWeGot;
+
+    });
+*/
+  } // /ngOnInit()
+
+  populateFormWithIngredient(myIndexPassedIn) {
+    console.log('well we are populating. hmm.');
+
+    const yeahIngredientWeSelectedButSoWhat: Ingredient = this.slService.getIngredient(myIndexPassedIn);
+
+    console.log('yeahIngredientWeSelectedButSoWhat ', yeahIngredientWeSelectedButSoWhat); // yeah {name: "ApplesWR__", amount: 5}
+
+    // So yeah the above is interesting but kinda useless.
+    // Instead, below, let's do some NEXT-ing. Ya! JoJo Rabbit!
+    this.slService.myIngredientToEditIndex.next(myIndexPassedIn);
+
   }
 
   ngOnDestroy(): void {
     this.myIngredientsChangedSubscription.unsubscribe();
+    // this.myIngredientSelectedToEditSubscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -10,10 +10,20 @@ export class ShoppingListService {
 */
   myIngredientsChangedSubject = new Subject<Ingredient[]>();
 
+  myIngredientToEdit = new Subject<Ingredient>();
+  myIngredientToEditIndex = new Subject<number>();
+
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient('ApplesWR__', 5),
+    new Ingredient('TomatoesWR__', 10),
   ];
+
+  getIngredient(myIndexPassedIn: number) {
+
+    const ingredientToReturn: Ingredient = this.ingredients[myIndexPassedIn];
+    this.myIngredientToEdit.next(ingredientToReturn);
+    return ingredientToReturn;
+  }
 
   getIngredients() {
     return this.ingredients.slice();
@@ -27,11 +37,53 @@ export class ShoppingListService {
     this.myIngredientsChangedSubject.next(this.ingredients.slice());
   }
 
+  updateIngredient(indexPassedIn: number, ingredientEdits: Ingredient) {
+    console.log('ING 02', ingredientEdits);
+    // {ingredient-nameName: "ketchup", amountName: 4}
+    /* Hmm. Odd (to me). ???
+    Why isn't TypeScript complaining, when I pass
+    my own object (See above) as the 2nd parameter
+    that is typed to 'Ingredient'?
+    That is:
+          IS NOW: not really Ingredient
+      { ingredient-nameName: "ApplesWR__ are goodf", amountName: 6}
+      NEEDS TO BECOME: Ingredient, n'est-ce pas?
+      Ingredient { name: "ApplesWR__ are goodf", amount: 6}
+     */
+
+    // Just whamma-jamma onto that index. Huh. No "map" etc. Hmm.
+    this.ingredients[indexPassedIn] = ingredientEdits;
+    console.log('ING 03 ', this.ingredients[indexPassedIn]);
+    // {ingredient-nameName: "ketchup", amountName: 4}
+
+    const newIngredientToUpdateDoneRight = new Ingredient(ingredientEdits['ingredient-nameName'], ingredientEdits['amountName']);
+    /* For: ingredientEdits.amountName << nope?
+    "Property 'amountName' does not exist on type 'Ingredient'"  (IDE/TypeScript warning ( ? ))
+    */
+    console.log('ING 04 ', newIngredientToUpdateDoneRight);
+    /* DIFFERS!
+    Ingredient {name: "TomatoesWR__older", amount: 10}
+     */
+
+   this.ingredients[indexPassedIn] = newIngredientToUpdateDoneRight;
+    console.log('ING 05 ', this.ingredients[indexPassedIn]);
+    /* Array of Ingredients, properly:
+    Ingredient {name: "ApplesWR__oldmaybe", amount: 33}
+     */
+
+    this.myIngredientsChangedSubject.next(this.ingredients.slice()); // send a copy (new, different, etc.)
+  } // /updateIngredient()
+
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
 /* RXJS SUBJECT NOW
     this.ingredientsChanged.emit(this.ingredients.slice());
 */
     this.myIngredientsChangedSubject.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(indexPassedIn: number) {
+    this.ingredients.splice(indexPassedIn, 1);
+    this.myIngredientsChangedSubject.next(this.ingredients.slice()); // makes new copy...
   }
 }
